69f0ce571ee3e0577575644faae7645f88d42575
P M <paula.melli2711@gmail.com>
2020-04-28 16:00:48 -0300

    Adds functions to sort the starting board
    The functions fill each space on the board with cards sorted randomly

---
+++ main.js
@@ -0,0 +1,7 @@
+function sortGame() {
+    const imageArray = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    fillBackOfCards();
+    sortFrontOfCardsRandomly(shuffleArray(imageArray));
+};
+
+
